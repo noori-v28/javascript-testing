45c9144a9d75abcd918a56c515bf236fcbff7e6a
noori-v28 <noorimumtaz659@gmail.com>
2025-06-20 11:08:44 +0000

    this is jus for learning

---
+++ .devcontainer/js besic/var.js
@@ -0,0 +1,11 @@
+const accountId = 765498
+let accountEmail = "noor@8967google.com"
+var accountPassward = "67543"
+accountCity = "kolkata"
+
+
+//console.log(accountId,accountEmail,accountPassward,accountCity) here we should use ([]),with this mathod here no error.
+
+// here error becouse account id is costent ,if any informetion is under the "const" it never change.
+
+console.log([accountId,accountCity,accountEmail,accountPassward])
