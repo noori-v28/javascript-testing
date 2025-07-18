00b61daf25242bd2c304790fb591f33a4e110769
noori-v28 <noorimumtaz659@gmail.com>
2025-07-18 20:43:22 +0530

    data type in js

---
+++ dataType.js
@@ -0,0 +1,26 @@
+"use strict"; //this method treats all js code as a new version
+
+//alert(7+7)//we are useing node.js nor browser
+
+//code should be readable
+
+
+let name ="kunnu"
+let age =20
+let isloggedIn= false
+let state;
+
+
+
+// data type =>
+    // string => " "
+    // boolean => true / false 
+    // null => standalon value
+    // undefined =>
+    // number => 2 to power 52
+    // bigint 
+    // symbol => unique
+
+    // object //in javascript we using "object" most pf the pleaces
+
+console.log(typeof false)
