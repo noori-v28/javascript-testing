fd6adaed1c635d34092ed3c36e7d8712144c0023
noori-v28 <noorimumtaz659@gmail.com>
2025-07-18 20:40:45 +0530

    test convertion in js

---
+++ conversionOpretion.js
@@ -0,0 +1,53 @@
+//let score = undefined
+
+//console.log(typeof undefined);
+//console.log(typeof (undefined))
+
+//let valueInNumber =number(undefined)
+
+//console.log(typeof valueInNumber);
+   
+
+// "33"=>33 (easyly convert)
+// "33adf" => output (nan)
+//true => 1; false => 0
+ 
+//****/ opretion**** //
+// let value= 3 
+
+// let negvelue = -value
+
+//console.log(negvalue);
+//console.log(2/2);
+//console.log(2-2);
+//console.log(2+2);
+//console.log(2*4);
+//console.log(2**3);
+//console.log(3%6);
+
+// let str1 ="hello"
+// let str2 =" puneet"
+
+// let str3 = str1+str2
+
+// console.log(str3) //besic situation
+
+console.log((+true));
+console.log("+");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
