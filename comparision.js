75c5a3e0bdb0eeaa88b1f2ffa339c40b84fa9b6c
noori-v28 <noorimumtaz659@gmail.com>
2025-07-18 20:37:48 +0530

    testing comparision

---
+++ comparision.js
@@ -0,0 +1,32 @@
+console.log(2 > 6);
+console.log(2 >= 7);
+console.log(3 < 6);      //its besic , no error and no problem ,best convarsion
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1 );     // here the problem is diffrent data type
+console.log("02"> 1);
+
+
+console.log(null > 0);
+console.log(null == 0);     // avoid thid type of conversion(comperision)
+console.log(null >= 0);
+
+
+
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+
+
+
+      // clean code is very valueble
+
+
+
+// === <= this threeple equal is strictly check any value and any data type
+
+console.log("2"===2);
+console.log();
